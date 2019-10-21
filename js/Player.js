ae6f87f25e44b7334343987d4a76e1f8fc5e5c7a
pratham lal <prathamlal370@gmail.com>
2019-10-21 16:54:29 +0530

    made rank as a property for cars

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -30,6 +31,18 @@ class Player {
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
+    })
+  }
+
+  static getcarsatend(){
+    database.ref('carsatend').on("value",(data)=>{
+      this.rank = data.val();
+    });
+  }
+
+  static updatecarsatend(rank){
+    database.ref('/').update({
+      carsatend:rank
     })
   }
 }
